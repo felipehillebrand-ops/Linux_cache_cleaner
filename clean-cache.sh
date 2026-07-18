bd0f0df6bf8c92817c2150252f482a982ce53f58
Felipe Hillebrand <felipehillebrand@gmail.com>
2026-07-18 21:15:24 +0100

    Add files via upload

--- clean-cache.sh
+++ clean-cache.sh
@@ -71,34 +71,6 @@ rm -rf ~/.cache/VirtualBox/* 2>/dev/null
 rm -rf ~/.cache/pip/* 2>/dev/null
 
 # Poetry
-#rm -rf ~/.cache/* 2>/dev/null
-
-# Google Chrome
-rm -rf ~/.config/google-chrome/Default/Cache/* 2>/dev/null
-rm -rf ~/.config/google-chrome/Default/Code\ Cache/* 2>/dev/null
-rm -rf ~/.config/google-chrome/Default/GPUCache/* 2>/dev/null
-rm -rf ~/.config/google-chrome/Default/Service\ Worker/CacheStorage/* 2>/dev/null
-rm -rf ~/.config/google-chrome/Default/Service\ Worker/ScriptCache/* 2>/dev/null
-
-# Firefox (Snap)
-rm -rf ~/snap/firefox/common/.cache/* 2>/dev/null
-rm -rf ~/snap/firefox/common/.mozilla/firefox/*/cache2/* 2>/dev/null
-rm -rf ~/snap/firefox/common/.mozilla/firefox/*/startupCache/* 2>/dev/null
-rm -rf ~/snap/firefox/common/.mozilla/firefox/*/thumbnails/* 2>/dev/null
-rm -rf ~/snap/firefox/common/.mozilla/firefox/*/serviceworker* 2>/dev/null
-
-# Slack
-rm -rf ~/.config/Slack/Cache/* 2>/dev/null
-rm -rf ~/.config/Slack/Code\ Cache/* 2>/dev/null
-rm -rf ~/.config/Slack/GPUCache/* 2>/dev/null
-
-# VSCode
-rm -rf ~/.config/Code/Cache/* 2>/dev/null
-rm -rf ~/.config/Code/CachedData/* 2>/dev/null
-rm -rf ~/.config/Code/logs/* 2>/dev/null
-rm -rf ~/.config/Code/GPUCache/* 2>/dev/null
-rm -rf ~/.config/Code/Service\ Worker/* 2>/dev/null
-rm -rf ~/.config/Code/Crashpad/* 2>/dev/null
 rm -rf ~/.cache/pypoetry/* 2>/dev/null
 
 # pytest
@@ -125,34 +97,6 @@ rm -rf ~/.cache/torch/* 2>/dev/null
 # Hugging Face
 rm -rf ~/.cache/huggingface/* 2>/dev/null
 
-#rm -rf ~/.cache/* 2>/dev/null
-
-# Google Chrome
-rm -rf ~/.config/google-chrome/Default/Cache/* 2>/dev/null
-rm -rf ~/.config/google-chrome/Default/Code\ Cache/* 2>/dev/null
-rm -rf ~/.config/google-chrome/Default/GPUCache/* 2>/dev/null
-rm -rf ~/.config/google-chrome/Default/Service\ Worker/CacheStorage/* 2>/dev/null
-rm -rf ~/.config/google-chrome/Default/Service\ Worker/ScriptCache/* 2>/dev/null
-
-# Firefox (Snap)
-rm -rf ~/snap/firefox/common/.cache/* 2>/dev/null
-rm -rf ~/snap/firefox/common/.mozilla/firefox/*/cache2/* 2>/dev/null
-rm -rf ~/snap/firefox/common/.mozilla/firefox/*/startupCache/* 2>/dev/null
-rm -rf ~/snap/firefox/common/.mozilla/firefox/*/thumbnails/* 2>/dev/null
-rm -rf ~/snap/firefox/common/.mozilla/firefox/*/serviceworker* 2>/dev/null
-
-# Slack
-rm -rf ~/.config/Slack/Cache/* 2>/dev/null
-rm -rf ~/.config/Slack/Code\ Cache/* 2>/dev/null
-rm -rf ~/.config/Slack/GPUCache/* 2>/dev/null
-
-# VSCode
-rm -rf ~/.config/Code/Cache/* 2>/dev/null
-rm -rf ~/.config/Code/CachedData/* 2>/dev/null
-rm -rf ~/.config/Code/logs/* 2>/dev/null
-rm -rf ~/.config/Code/GPUCache/* 2>/dev/null
-rm -rf ~/.config/Code/Service\ Worker/* 2>/dev/null
-rm -rf ~/.config/Code/Crashpad/* 2>/dev/null
 # Matplotlib
 rm -rf ~/.cache/matplotlib/* 2>/dev/null
 
@@ -167,32 +111,4 @@ rm -rf ~/.npm/_cacache/* 2>/dev/null
 # Lixo
 rm -rf ~/.local/share/Trash/* 2>/dev/null
 
-echo "✅ Limpeza concluída!"#rm -rf ~/.cache/* 2>/dev/null
-
-# Google Chrome
-rm -rf ~/.config/google-chrome/Default/Cache/* 2>/dev/null
-rm -rf ~/.config/google-chrome/Default/Code\ Cache/* 2>/dev/null
-rm -rf ~/.config/google-chrome/Default/GPUCache/* 2>/dev/null
-rm -rf ~/.config/google-chrome/Default/Service\ Worker/CacheStorage/* 2>/dev/null
-rm -rf ~/.config/google-chrome/Default/Service\ Worker/ScriptCache/* 2>/dev/null
-
-# Firefox (Snap)
-rm -rf ~/snap/firefox/common/.cache/* 2>/dev/null
-rm -rf ~/snap/firefox/common/.mozilla/firefox/*/cache2/* 2>/dev/null
-rm -rf ~/snap/firefox/common/.mozilla/firefox/*/startupCache/* 2>/dev/null
-rm -rf ~/snap/firefox/common/.mozilla/firefox/*/thumbnails/* 2>/dev/null
-rm -rf ~/snap/firefox/common/.mozilla/firefox/*/serviceworker* 2>/dev/null
-
-# Slack
-rm -rf ~/.config/Slack/Cache/* 2>/dev/null
-rm -rf ~/.config/Slack/Code\ Cache/* 2>/dev/null
-rm -rf ~/.config/Slack/GPUCache/* 2>/dev/null
-
-# VSCode
-rm -rf ~/.config/Code/Cache/* 2>/dev/null
-rm -rf ~/.config/Code/CachedData/* 2>/dev/null
-rm -rf ~/.config/Code/logs/* 2>/dev/null
-rm -rf ~/.config/Code/GPUCache/* 2>/dev/null
-rm -rf ~/.config/Code/Service\ Worker/* 2>/dev/null
-rm -rf ~/.config/Code/Crashpad/* 2>/dev/null
-
+echo "✅ Limpeza concluída!"
